e19f6fa311898e726f741967cff58f43430e6526
SJISU <zy0508@naver.com>
2021-09-29 21:36:56 +0900

    20210929

--- WebSpring03_jdbcTemplate/src/main/java/com/bitcamp/app/board/BoardController.java
+++ WebSpring03_jdbcTemplate/src/main/java/com/bitcamp/app/board/BoardController.java
@@ -3,6 +3,7 @@ package com.bitcamp.app.board;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +11,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BoardController {
-	public BoardDAO dao = new BoardDAO();
+	   @Autowired  // @Inject
+	    public BoardDAO dao;
+	   //public BoardDAO dao = new BoardDAO();
+
 	
 	@RequestMapping("/board/list")
 	public ModelAndView list() { //리스트불러오는
@@ -37,5 +41,46 @@ public class BoardController {
 		return mav;
 	
 	}
-	
+	//글내용보기
+	 @RequestMapping("/board/view")
+	   public ModelAndView view(int no) {
+	      ModelAndView mav = new ModelAndView();
+	      mav.addObject("vo", dao.oneRecord(no));
+	      mav.setViewName("board/view");
+	      return mav;
+	      
+	   }
+	 @RequestMapping("/board/edit")
+	 public ModelAndView edit(int no) {
+		 ModelAndView mav = new ModelAndView();
+		 mav.addObject("vo",dao.oneRecord(no));
+		 mav.setViewName("board/edit");
+		 return mav;
+	 }
+	 //수정이끝나고나면 -> 성공,실패
+	 @RequestMapping(value="/board/editOk",method=RequestMethod.POST)
+	 public ModelAndView editOk(BoardVO vo) {
+		 ModelAndView mav = new ModelAndView();
+		 mav.addObject("no",vo.getNo());
+		 
+		 int result = dao.boardUpdate(vo);//디비작업		 
+		 if(result>0){//수정시 글내용보기로
+			 mav.setViewName("redirect:view"); // 앞에경로빼고 바로 그페이지로
+		 }else{//수정실패시 글수정폼으로
+			 mav.setViewName("redirect:edit");
+		 }
+		 return mav;
+	 }
+	 @RequestMapping("/board/del")
+	    public ModelAndView del(int no) {
+	         ModelAndView mav = new ModelAndView();
+	         int result = dao.boardDelete(no);
+	         if(result>0) {//삭제한경우
+	            mav.setViewName("redirect:list");
+	         }else {//삭제못한경우
+	            mav.addObject("no",no);       
+	            mav.setViewName("redirect:view");
+	         }
+	         return mav;
+	    }
 }
--- WebSpring03_jdbcTemplate/src/main/java/com/bitcamp/app/board/BoardDAO.java
+++ WebSpring03_jdbcTemplate/src/main/java/com/bitcamp/app/board/BoardDAO.java
@@ -11,47 +11,59 @@ import org.springframework.jdbc.core.PreparedStatementSetter;
 import com.bitcamp.app.Constants;
 
 public class BoardDAO implements BoardDAOImpl {
-	public JdbcTemplate template;
-	public BoardDAO() {
-		template = Constants.template;
-	}
-	@Override
-	   public List<BoardVO> allRecord() {
-	      String sql = "select no, subject, userid, hit, to_char(writedate,'MM-DD HH:MI') writedate "
-	            + " from board order by no desc";
-	      return template.query(sql, new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
-	   }
-	@Override
-	public BoardVO oneRecord(int no) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public int boardUpdate(BoardVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int boardInsert(final BoardVO vo) { //글쓰기작성버튼누르면발생하는 //익명의내부클래스안에서도 사용하기위해 final 붙여준다
-		
-		String sql="insert into board(no, userid, subject, content, ip) values(boardsq.nextval, ?, ?, ?, ?)";
-		return template.update(sql, new PreparedStatementSetter() {//preparedStatementSetter->익명 선택하면 오버라이딩하는메소드
-			
-			//익명의 내부클래스
-			@Override
-			public void setValues(PreparedStatement ps) throws SQLException {//물음표셋팅
-				ps.setString(1, vo.getUserid());
-				ps.setString(2,vo.getSubject());
-				ps.setString(3, vo.getContent());
-				ps.setString(4,vo.getIp());
-				
-			}
-		} );
-		
-	}
-	@Override
-	public int boardDelete(int no) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+   public JdbcTemplate template;
+   public BoardDAO() {
+      template = Constants.template;
+   }
+   // 전체 리스트 => 전체 레코드 선택
+   @Override
+   public List<BoardVO> allRecord() {
+      String sql = "select no, subject, userid, hit, to_char(writedate, 'MM-DD HH:MI') as writedate from board order by no desc";
+      // select 쿼리문에 물음표가 없으면 이걸 쓰면 될듯
+      return template.query(sql, new BeanPropertyRowMapper<BoardVO>(BoardVO.class));
+   };
+   
+   // 글내용 보기, 수정 => 레코드 하나 선택
+   @Override
+   public BoardVO oneRecord(int no) {
+      String sql = "select no, userid, subject, content, hit, writedate from board where no=?";
+      return template.queryForObject(sql, new BeanPropertyRowMapper<BoardVO>(BoardVO.class), no);
+   };
+   
+   // 글 수정
+   @Override
+   public int boardUpdate(BoardVO vo) {
+      String sql = "update board set content=?, subject=? "
+      				+ " where no=?" ;
+    
+      return  template.update(sql, vo.getContent(), vo.getSubject(), vo.getNo());//1:수정 0:수정못함
+      
+   };
+   
+   // 글 등록
+   @Override
+   public int boardInsert(final BoardVO vo) {
+      String sql = "insert into board(no, userid, subject, content, ip) values(boardsq.nextval, ?, ?, ?, ?)";
+      
+      // 추가할 때는 update!
+      return template.update(sql, new PreparedStatementSetter() {
+         @Override
+         public void setValues(PreparedStatement ps) throws SQLException {
+            // update의 두 번째 인자인 PreparedStatementSetter는 익명의 내부 클래스로 작성
+            // 따라서 BoardVO 객체를 쓰기 위해서는 boardInsert로 전달되는 BoardVO의 값을 final로 바꿔 주어야 한다.
+            ps.setString(1, vo.getUserid());
+            ps.setString(2, vo.getSubject());
+            ps.setString(3, vo.getContent());
+            ps.setString(4, vo.getIp());
+         }
+      });
+   };
+   
+   // 글 삭제
+   @Override
+   public int boardDelete(int no) {
+	   String sql ="delete from board where no=?";
+	   return template.update(sql,no);
+     
+   }
 }
